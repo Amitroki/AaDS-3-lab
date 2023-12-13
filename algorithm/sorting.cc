d400b832e13244e1bf3451117652f2f4f0da5952
Алексей Русяев <RusyaevAA@outlook.com>
2023-12-14 00:09:39 +0400

    working quick sort

--- algorithm/sorting.cc
+++ algorithm/sorting.cc
@@ -7,26 +7,62 @@ namespace algorithm {
 	struct stats {
 		size_t comparison_count = 0;
 		size_t copy_count = 0;
-
+		stats() : comparison_count(0), copy_count(0) {}
+		stats(const stats& other) {
+			comparison_count = other.comparison_count;
+			copy_count = other.copy_count;
+		}
+		stats& operator=(const stats& other) {
+			comparison_count = other.comparison_count;
+			copy_count = other.copy_count;
+		}
 	};
+	void swap(int& a, int& b, stats& stats) {
+		int temp = a;
+		a = b;
+		b = temp;
+		stats.copy_count += 3;
+	}
 	template <typename T>	
 	stats bubble_sort(std::vector<T> & arr) {
 		stats sorting_results;
 		int n = arr.size();
 		for (int i = 0; i < n - 1; i++) {
 			for (int j = 0; j < n - i - 1; j++) {
-				sorting_results.comparison_count += 1;
+				sorting_results.comparison_count++;
 				if (arr[j] > arr[j + 1]) {
-					sorting_results.copy_count += 3;
-					swap(arr[j], arr[j + 1]);
+					swap(arr[j], arr[j + 1], sorting_results);
 				}
 			}
 		}
 		return sorting_results;
 	}
+	int partition(std::vector<int>& arr, int low, int high, stats& stats) {
+		int pivot = arr[high];
+		int i = (low - 1);
+		for (int j = low; j <= high - 1; j++) {
+			if (arr[j] <= pivot) {
+				i++;
+				swap(arr[i], arr[j], stats);
+			}
+			stats.comparison_count++;
+		}
+		swap(arr[i + 1], arr[high], stats);
+		return (i + 1);
+	}
+	stats quickSort(std::vector<int>& arr, int low, int high) {
+		stats stat;
+		if (low < high) {
+			int pi = partition(arr, low, high, stat);
+			stats left_stats = quickSort(arr, low, pi - 1);
+			stats right_stats = quickSort(arr, pi + 1, high);
+			stat.comparison_count+= left_stats.comparison_count + right_stats.comparison_count;
+			stat.copy_count += left_stats.copy_count + right_stats.copy_count;
+		}
+		return stat;
+	}
 	template<typename T>
-	std::ostream& operator << (std::ostream& os, const std::vector<T> vec)
-	{
+	std::ostream& operator << (std::ostream& os, const std::vector<T> vec) {
 		os << "{ ";
 		for (int i = 0; i < vec.size(); i++) {
 			if (i < vec.size() - 1)
--- tests/sorting_tests.cc
+++ tests/sorting_tests.cc
@@ -5,14 +5,26 @@ using namespace algorithm;
 using namespace std;
 
 TEST(Sorting, Bubble1) {
-	std::vector<int> a{ 1, 3, 2, 6, 7, 5, 4, 8, 10, 9 };
+	std::vector<int> a{ 31, 2, 30, 18, 34, 17, 30, 9, 30, 11, 4, 14, 29, 5, 21, 15, 17, 35, 37, 26, 38, 30, 40, 17, 29, 21, 7, 19, 38, 16 };
 	stats b = bubble_sort(a);
 	cout << a;
 	cout << b;
 }
 TEST(Sorting, Bubble2) {
-	std::vector<int> a{ 31, 2, 30, 18, 34, 17, 30, 9, 30, 11, 4, 14, 29, 5, 21, 15, 17, 35, 37, 26, 38, 30, 40, 17, 29, 21, 7, 19, 38, 16 };
+	std::vector<int> a{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
 	stats b = bubble_sort(a);
+	cout << a;
+	cout << b;
+}
+TEST(Sorting, Quick1) {
+	std::vector<int> a{ 31, 2, 30, 18, 34, 17, 30, 9, 30, 11, 4, 14, 29, 5, 21, 15, 17, 35, 37, 26, 38, 30, 40, 17, 29, 21, 7, 19, 38, 16 };
+	stats b = quickSort(a, 0, a.size() - 1);
+	cout << a;
+	cout << b;
+}
+TEST(Sorting, Quick2) {
+	std::vector<int> a{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+	stats b = quickSort(a, 0, a.size() - 1);
 	cout << a;
 	cout << b;
 }
